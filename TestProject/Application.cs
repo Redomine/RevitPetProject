955da48d2bac3508814ce64509a633af662a49ea
Redomine <romankaev@gmail.com>
2022-12-27 11:04:12 +0300

    Добавьте файлы проекта.

---
+++ TestProject/Application.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    internal class Application
+    {
+        public Result OnStartUp(UIControlledApplication application)
+        {
+            string assemblyLocation = Assembly.GetExecutingAssembly().Location,
+                iconsDirPath = Path.GetDirectoryName(assemblyLocation) + @"\icons",
+                tabName = "Тестовый проект";
+
+            application.CreateRibbonTab(tabName);
+
+            RibbonPanel panel = application.CreateRibbonPanel(tabName, "Первый плагин");
+
+            PushButtonData buttonData = new PushButtonData(nameof(MyTestClass), "Приветствие", assemblyLocation, iconsDirPath);
+
+
+        }
+    }
+}
---
+++ TestProject/MyTestClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace TestProject
+{
+    [Transaction(TransactionMode.Manual)]
+    public class MyTestClass : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Приветствие", "Привет мир");
+
+            return Result.Succeeded;
+        }
+    }
+}
